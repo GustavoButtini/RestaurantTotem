ec4a834618e8ce30222bbad4c522f929dc374670
GustavoButtini <gustavo.buttini@hotmail.com>
2025-02-21 19:47:36 -0300

    feat: Configuring header Buttons

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { consumptionMethod } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
@@ -57,5 +58,6 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: res!.id,
     },
   });
+  revalidatePath(`/${input.slug}/orders`);
   redirect(`/${input.slug}/orders?cpf=${removeCpfPunctionation(input.customerCPF)}`)
 };
